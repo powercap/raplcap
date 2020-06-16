b64406337986ee77b9b9fd0a57bae6cd2fad7b84
Connor Imes <connor.k.imes@gmail.com>
2020-06-15 21:34:44 -0400

    raplcap-msr: prefer the term 'package' rather than 'socket'

--- msr/raplcap-msr.h
+++ msr/raplcap-msr.h
@@ -18,74 +18,74 @@ extern "C" {
  * Check if a zone is clamped.
  *
  * @param rc
- * @param socket
+ * @param pkg
  * @param zone
  * @return 0 if not clamped, 1 if clamped, a negative value on error
  */
-int raplcap_msr_is_zone_clamped(const raplcap* rc, uint32_t socket, raplcap_zone zone);
+int raplcap_msr_is_zone_clamped(const raplcap* rc, uint32_t pkg, raplcap_zone zone);
 
 /**
  * Clamp/unclamp a zone.
  * Note: clamping is automatically set when a zone is enabled.
  *
  * @param rc
- * @param socket
+ * @param pkg
  * @param zone
  * @param clamp
  * @return 0 on success, a negative value on error
  */
-int raplcap_msr_set_zone_clamped(const raplcap* rc, uint32_t socket, raplcap_zone zone, int clamped);
+int raplcap_msr_set_zone_clamped(const raplcap* rc, uint32_t pkg, raplcap_zone zone, int clamped);
 
 /**
  * Check if a zone is locked.
  *
  * @param rc
- * @param socket
+ * @param pkg
  * @param zone
  * @return 0 if unlocked, 1 if locked, a negative value on error
  */
-int raplcap_msr_is_zone_locked(const raplcap* rc, uint32_t socket, raplcap_zone zone);
+int raplcap_msr_is_zone_locked(const raplcap* rc, uint32_t pkg, raplcap_zone zone);
 
 /**
  * Lock a zone.
  * Note: once locked, a zone cannot be unlocked until CPU is reset.
  *
  * @param rc
- * @param socket
+ * @param pkg
  * @param zone
  * @return 0 on success, a negative value on error
  */
-int raplcap_msr_set_zone_locked(const raplcap* rc, uint32_t socket, raplcap_zone zone);
+int raplcap_msr_set_zone_locked(const raplcap* rc, uint32_t pkg, raplcap_zone zone);
 
 /**
  * Get the time units for a zone in seconds.
  *
  * @param rc
- * @param socket
+ * @param pkg
  * @param zone
  * @return Seconds on success, a negative value on error
  */
-double raplcap_msr_get_time_units(const raplcap* rc, uint32_t socket, raplcap_zone zone);
+double raplcap_msr_get_time_units(const raplcap* rc, uint32_t pkg, raplcap_zone zone);
 
 /**
  * Get the power units for a zone in Watts.
  *
  * @param rc
- * @param socket
+ * @param pkg
  * @param zone
  * @return Watts on success, a negative value on error
  */
-double raplcap_msr_get_power_units(const raplcap* rc, uint32_t socket, raplcap_zone zone);
+double raplcap_msr_get_power_units(const raplcap* rc, uint32_t pkg, raplcap_zone zone);
 
 /**
  * Get the energy units for a zone in Joules.
  *
  * @param rc
- * @param socket
+ * @param pkg
  * @param zone
  * @return Joules on success, a negative value on error
  */
-double raplcap_msr_get_energy_units(const raplcap* rc, uint32_t socket, raplcap_zone zone);
+double raplcap_msr_get_energy_units(const raplcap* rc, uint32_t pkg, raplcap_zone zone);
 
 #ifdef __cplusplus
 }
